d165ceafeeb9e1de81d25399b3c2d6eed210f7fb
Bruno <wi22b058@technikum-wien.at>
2023-06-28 19:43:34 +0200

    Added feature modify

---
+++ src/main/java/com/example/exam/Modify.java
@@ -0,0 +1,39 @@
+package com.example.exam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Modify {
+
+
+    @RestController
+    public class StringModifyController {
+
+        private String lastModifiedWord = "";
+        private int lastModifiedWordLength = 0;
+
+        @GetMapping("/api/modify/length")
+        public int getLastModifiedWordLength() {
+            return lastModifiedWordLength;
+        }
+
+        @GetMapping("/api/modify")
+        public String modifyString(@RequestParam String string) {
+            StringBuilder modifiedString = new StringBuilder();
+            for (int i = 0; i < string.length(); i++) {
+                if (i % 2 == 0) {
+                    modifiedString.append(Character.toLowerCase(string.charAt(i)));
+                } else {
+                    modifiedString.append(Character.toUpperCase(string.charAt(i)));
+                }
+            }
+            lastModifiedWord = modifiedString.toString();
+            lastModifiedWordLength = lastModifiedWord.length();
+            return lastModifiedWord;
+        }
+    }
+
+
+
+}
